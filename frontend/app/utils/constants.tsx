97e5ef86f88057cd6d2ce35be5d43db63e6be186
Tommy SIN <64023236+TommySinPolyU@users.noreply.github.com>
2024-05-20 16:20:02 +0800

    Update constants.tsx

--- frontend/app/utils/constants.tsx
+++ frontend/app/utils/constants.tsx
@@ -1,5 +1,5 @@
 /*const apiBasePath = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : `http://localhost:3000`;*/
-const apiBasePath = `https://chat-langchainjs-frontend-delta.vercel.app`;
+const apiBasePath = "https://chat-langchainjs-frontend-delta.vercel.app";
 export const apiBaseUrl = `${apiBasePath}/api`;
